1e71f5b90c32657e79dafdb3214c36530e98bdc8
Daniel Heiniger <daniel.r.heiniger@gmail.com>
2020-09-10 19:30:46 -0500

    Initial Commit

---
+++ env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [redeemer-mobile.ios.core :as core]))
+
+ (core/init)
+
+
---
+++ init-android.sh
@@ -0,0 +1,3 @@
+re-natal use-android-device real &
+re-natal use-figwheel &
+lein figwheel android
